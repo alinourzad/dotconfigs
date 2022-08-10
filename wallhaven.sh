15f30ad13ca7b28563b9df5e3fa2e2350f4e74ce
alinourzad <alocersade@gmail.com>
2022-08-10 12:18:58 +0200

    changed catergory of wallhaven wallpapers

--- wallhaven.sh
+++ wallhaven.sh
@@ -6,7 +6,7 @@ which curl >/dev/null || sudo apt install -y curl
 
 # categories 100/101/111* (general/anime/people) Turn categories on(1) or off(0)
 # purity     100/110/111  (sfw/sketchy/nsfw)     
-curl -sX GET 'https://wallhaven.cc/api/v1/search?ratios=16x9&purity=100&categories=010&sorting=views' | jq .data[].path | cut -d '"' -f 2 | tee uris.txt
+curl -sX GET 'https://wallhaven.cc/api/v1/search?ratios=16x9&purity=100&categories=100&sorting=views' | jq .data[].path | cut -d '"' -f 2 | tee uris.txt
 #cat uris.txt
 directory="$HOME/Wallpapers"
 [ ! -d ${directory} ] && mkdir ${directory}
