7ee18322e51133b159a808004e02c93024dbe197
alinourzad <alocersade@gmail.com>
2022-09-06 15:28:45 +0200

    added picom compilation and extra configurations

--- install.sh
+++ install.sh
@@ -10,7 +10,31 @@ print_message () {
 }
 
 print_message "INSTALLING REQUIRED PACKAGES"
+
+install_picom() {
+    sudo apt-get install -qq libxext-dev libxcb1-dev \
+	 libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
+	 libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev \
+	 libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
+	 libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev \
+	 libdbus-1-dev libconfig-dev libgl1-mesa-dev \
+	 libpcre2-dev libevdev-dev uthash-dev \
+	 libev-dev libx11-xcb-dev \
+	 libpcre3-dev
+    [[ -d picom ]] && rm -rf picom
+    pushd .
+    gh repo clone jonaburg/picom
+    cd picom
+    git submodule update --init --recursive
+    meson --buildtype=release . build
+    ninja -C build
+    ninja -C build install
+    popd
+    rm -rf picom
+}
+
 sudo apt-get install -qq xinit \
+     meson ninja-build \
 	rofi \
 	gh git \
 	polybar \
@@ -22,30 +46,29 @@ sudo apt-get install -qq xinit \
 	qutebrowser \
 	vim \
 	pulseaudio \
-	picom \
 	i3lock-fancy \
 	dunst \
 	nnn \
+	brightnessctl \
 	python3-pip
+
+which picom > /dev/null || install_picom
+
 print_message "INSTALLING PACKAGES DONE."
 
 create_link(){
-	if [[ ! -d ~/.config ]]
-	then
-		mkdir ~/.config
-	fi
+	[[ ! -d ~/.config ]] && mkdir .config
 	
-	ln -s $PWD/bspwm   ~/.config/bspwm   || echo 'bspwm exists'
-	ln -s $PWD/sxhkd   ~/.config/sxhkd   || echo 'sxhkd already there'
-	ln -s $PWD/polybar ~/.config/polybar || echo 'polybar already there'
+	ln -s $PWD/bspwm   ~/.config || echo 'bspwm exists'
+	ln -s $PWD/sxhkd   ~/.config || echo 'sxhkd already there'
+	ln -s $PWD/polybar ~/.config || echo 'polybar already there'
+	ln -s $PWD/picom   ~/.config || echo 'picom already exists'
+
+	[[ ! -d ~/.local/bin ]] && mkdir ~/.local/bin
+	ln -s $PWD/scripts/pavolume.sh ~/.local/bin/pavolume.sh || echo 'pavolume.sh exists'
 }
 
-if [[ ! -L ~/.config/bspwm ]]
-then
-	print_message 'CREATING LINKS'
-	create_link
-	print_message 'CREATING LINGS DONE.'
-fi
+create_link
 
 echo 'exec bspwm' > ~/.xsession
 
@@ -71,10 +94,4 @@ fi
 if [[ ! -L ~/.config/dunst ]];
 then
 	ln -s $PWD/dunst ~/.config/dunst
-fi
-
-# PICOM CONFIGURATION
-if [[ ! -L ~/.config/picom ]];
-then
-	ln -s $PWD/picom ~/.config/picom
 fi
